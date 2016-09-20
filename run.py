b443b5f6180689b506a764edb1f5939f4b99c1a5
Billy Wong <bwong5@stuy.edu>
2016-09-20 18:23:44 -0400

    added routes

---
+++ run.py
@@ -0,0 +1,24 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def helloworld():
+    return "No mi"
+
+@app.route("/secret")
+def secsec():
+    return '''
+    <html>
+    <body>
+    Hello! Can you read this?
+    </body>
+    </html>
+    '''
+
+@app.route("/notsecret")
+def something():
+    return "you cannot read this"
+
+
+if __name__ == "__main__":
+    app.run()
